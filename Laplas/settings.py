be6dbf604d804cda5685829baa4b273c86dc410f
Yasirooz <testwebsite1244@gmail.com>
2022-07-06 14:05:17 +0430

    fixed

--- Laplas/settings.py
+++ Laplas/settings.py
@@ -222,8 +222,8 @@ AUTH_USER_MODEL = 'account.User'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'reallaplace@gmail.com'
-EMAIL_HOST_PASSWORD = 'qikwezvxvubcsuqc'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 # qikwezvxvubcsuqc
 
